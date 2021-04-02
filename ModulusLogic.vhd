d2b3cac138e21f5727542018ee7a5b58c752c173
Nick Jacks <45337815+njacks1@users.noreply.github.com>
2021-04-02 01:06:36 -0400

    Fixed synthesizing error

--- ModulusLogic.vhd
+++ ModulusLogic.vhd
@@ -1,24 +1,24 @@
 library ieee;
 use ieee.std_logic_1164.all;
-use ieee.std_logic_unsigned.all;
-use ieee.std_logic_arith.all;
 use ieee.numeric_std.all;
 
 entity ModulusLogic is  
 port( 
-    input_lreg  :   in std_logic_vector(13 downto 0);  --Input from I registers
+    input_lreg  :   in std_logic_vector(13 downto 0);  --Input from L registers
     input_adder  :   in std_logic_vector(13 downto 0);  --Input from M registers
     output  :   out std_logic_vector(13 downto 0)); --Output to Modulous Logic
 end ModulusLogic;
 
 architecture behav of ModulusLogic is
-    i := to_integer(unsigned(input_adder));
-    L := to_integer(unsigned(input_lreg));
-
     begin
-        process(input_lreg, input_adder)
+        process(input_adder)
+            variable i : integer;
+	    variable L : integer;
+            variable o : integer;
             begin
-                if(L = '0') then
+		i := to_integer(unsigned(input_adder));
+            	L := to_integer(unsigned(input_lreg));
+                if(L = 0) then
                     output <= input_adder;  --Linear
                 else    --Circular Buffer
                     o := i mod(L);
